95fee4b55dd48cd6c23e8eeadcf0c71e55083337
Aher Chetan <69471574+AherChetan1999@users.noreply.github.com>
2023-07-11 20:43:05 +0530

    simple 2 task in js
    Deliverables
    Write an arrow function which returns the product of two numbers
    Watch video 6 as well and create a student object

---
+++ simple.js
@@ -0,0 +1,16 @@
+
+// problem 1
+const product=(a,b)=>a*b;
+
+console.log("product of 2 numbers: "+product(2,4));
+
+//problem 2
+
+const student={
+    first_name:"chetan",
+    last_name:"aher",
+    age:24,
+    roll_number:101
+};
+
+console.log(student);
