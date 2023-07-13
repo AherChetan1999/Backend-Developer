3c5192ec5fab96b1f6734df749ee98afe1a7986c
Aher Chetan <69471574+AherChetan1999@users.noreply.github.com>
2023-07-13 12:34:21 +0530

    server 4000

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express= require("express");
 const app= express();
 
 app.get("/", (req,res)=>{
-    res.send("Hello World...!!")
+    res.send("Chetan Aher")
 })
 
-app.listen(2000, ()=>{
-    console.log("server started....!!");
+app.listen(4000, ()=>{
+    console.log("Hii chetan server is  started....!!");
 })
