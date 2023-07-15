29b1284b9f45adda0cfee1489cfe63e12ab3a9c6
Aher Chetan <69471574+AherChetan1999@users.noreply.github.com>
2023-07-15 15:31:12 +0530

    Routers $ Filters

---
+++ Routers $ Filters/index.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const body_parser= require('body-parser')
+const admin=require("./routers/admin");
+const shop=require("./routers/shop");
+
+const app=express();
+
+app.use(body_parser.urlencoded({extended:false}));
+
+app.use(admin);
+app.use(shop);
+
+app.use((req,res)=>{
+     res.status(404).send('<h1>Page Not Found.</h1>')
+});
+
+
+app.listen(2000);
